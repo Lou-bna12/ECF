d315d7d24c0b1224d73a66ca3abd8b38d86fb020
Loubna Sellam <loubnasellam49@gmail.com>
2023-06-19 10:15:40 +0200

    git commit

--- admin_users.php
+++ admin_users.php
@@ -1,8 +1,24 @@
 <?php
 include('config.php');
 
+session_start();
+
+$admin_id = $_SESSION['name'];
+
+if(!isset($admin_id)){
+  header('location:login.php');
+}
+
+if(isset($_GET['delete'])){
+  $delete_id = $_GET['delete'];
+  mysqli_query($conn, "DELETE FROM users WHERE id = '$delete_id'") or die(mysqli_error($conn));
+    header("Location:admin_users.php");
+}
+
 
 ?>
+
+
 <!DOCTYPE html>
 <html lang="fr_FR">
 <head>
@@ -12,10 +28,9 @@ include('config.php');
 
     <!-- font awesome -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-   
-      <!--  css dossier admin -->
-
+         <!--  css dossier admin -->
       <link rel="stylesheet" href="Garage Parrot/css/admin_style.css">
+
 </head>
 <body>
 
@@ -31,13 +46,11 @@ include('config.php');
         <div class="box">
           <p> Nom : <span><?php echo $fetch_users['name']; ?></span> </p>
           <p> Adresse email : <span><?php echo $fetch_users['email']; ?></span> </p>
-          <p>Le type d'utilisateur : <span><?php echo $fetch_users['user_type']; ?></span> </p>
-          <a href="admin_orders.php?delete=<?php echo $fetch_orders['id']; ?>" onclick="return confirm('Supprimer cette commande?');"
-     class="delete-btn">Supprimer</a>
+          <p>Le type d'utilisateur : <span style="color:<?php if($fetch_users['user_type'] == 'admin'){ echo 'var(--orange)'; } ?>"><?php echo $fetch_users['user_type']; ?></span> </p>
+          <a href="admin_users.php?delete=<?php echo $fetch_users['id']; ?>" onclick="return confirm('Supprimer cet utilisateur?');" class="delete-btn">Supprimer</a>
     </div>
     <?php
-    
-       };
+      };
     
     ?>
 
