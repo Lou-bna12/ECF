98046e9d2ead9cc8b91a88ae1255a9071438a0db
Loubna Sellam <loubnasellam49@gmail.com>
2023-06-20 21:35:44 +0200

    git commit

--- about.php
+++ about.php
@@ -30,7 +30,12 @@ if(!isset($user_id)){
 <body>
 
 <?php include('header.php'); ?>
-   
+
+<div class="heading">
+
+   <h3>À propos</h3>
+<p><a href="home.php">Accueil</a> / À propos</p>
+</div> 
 
 <section class="about">
 
@@ -62,15 +67,163 @@ if(!isset($user_id)){
 
 </section>
 
-<section class="home-contact">
+<section class="reviews">
+<h1 class="title">Les commentaires des clients</h1>
 
-<div class="content">
-   <h3>Avez-vous des questions ?</3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    <a href="contact.php" class="white-btn">Contacter-nous</a>
+<div class="box-container">
+
+<div class="box">
+      <img src="Garage Parrot/images/pic-1.png" alt="">
+   <p>Garage sérieux et responsabilité professionnelle.</p>
+   <div class="stars">
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star-half-alt"></i>
+   </div>
+   <h3>Mickaël BEAUMANN</h3>
 </div>
 
+<div class="box">
+   <img src="Garage Parrot/images/pic-2.png" alt="">
+   <p>Super, garage.Bravo à toutes l'équipe.</p>
+   <div class="stars">
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star-half-alt"></i>
+   </div>
+   <h3>Maryline SUZINEAU</h3>
+</div>
 
+<div class="box">
+   <img src="Garage Parrot/images/pic-3.png" alt="">
+ <p>Bon garage ,bon accueil, réparations bien tenue...</p>
+   <div class="stars">
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+   </div>
+   <h3>Serge RAULT</h3>
+</div>
+
+<div class="box">
+   <img src="Garage Parrot/images/pic-4.png" alt="">
+   <p>Excellent accueil, rdv très rapide.</p>
+   <div class="stars">
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+   </div>
+   <h3>Michélle MERCEUR</h3>
+</div>
+
+<div class="box">
+   <img src="Garage Parrot/images/pic-5.png" alt="">
+   <p> Super accueil avec un suivi de qualité.</p>
+   <div class="stars">
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star-half-alt"></i>
+   </div>
+   <h3>Thierry BONNEFOI</h3>
+</div>
+
+<div class="box">
+   <img src="Garage Parrot/images/pic-6.png" alt="">
+   <p>Bon accueil travaux propre.</p>
+   <div class="stars">
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star"></i>
+    <i class="fas fa-star-half-alt"></i>
+   </div>
+   <h3>Auréline JEAN</h3>
+</div>
+
+</div>
+
+</section>
+
+<section class="teams">
+
+<h1 class="title">Notre meilleure équipe</h1>
+
+   <div class="box-container">
+
+   <div class="box">
+      <img src="Garage Parrot/images/team-1.jpg" alt="">
+      <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>John DOE<h3>
+      </div>
+   
+   <div class="box">
+      <img src="Garage Parrot/images/team-2.jpg" alt="">
+      <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>Stephanie REBELLE<h3>
+     </div>
+<div class="box">
+      <img src="Garage Parrot/images/team-3.jpg" alt="">
+      <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>Alexis MARTINEZ<h3>
+      </div>
+ <div class="box">
+      <img src="Garage Parrot/images/team-4.jpg" alt="">
+      <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>Cedric DASILVA<h3>
+      </div>
+      <div class="box">
+      <img src="Garage Parrot/images/team-5.jpg" alt="">
+      <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>Adam DASIA<h3>
+</div>
+<div class="box">
+      <img src="Garage Parrot/images/team-6.jpg" alt="">
+      <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        </div>
+        <h3>Mark MARTINE<h3>
+ </div>
+
+
+</div>
 </section>
 
 
